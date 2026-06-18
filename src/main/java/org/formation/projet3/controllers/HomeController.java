2068ec17fa6ffe6c03bb0bb1f7c3e4aeccf8a942
Mohammed REZGUI <m.rezgui06@gmail.com>
2026-06-18 10:54:46 +0200

    feat: add produces type (personnebean serialisation)

--- src/main/java/org/formation/projet3/controllers/HomeController.java
+++ src/main/java/org/formation/projet3/controllers/HomeController.java
@@ -1,6 +1,9 @@
 package org.formation.projet3.controllers;
 
+import org.formation.projet3.personne.PersonneBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @Autowired
+    private PersonneBean personneBean;
 
     @GetMapping("/")
     public String home() {
@@ -46,6 +52,34 @@ public class HomeController {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body("Ressource introuvable");
+    }
+
+    @GetMapping("/unothorized")
+    public ResponseEntity<String> unothorized() {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body("non autorisé");
+    }
+
+    @GetMapping("/forbidden")
+    public ResponseEntity<String> forbidden() {
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body("ressource interdite");
+    }
+
+    @GetMapping(path = "/personnejson", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<PersonneBean> getPersonneJSON() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(personneBean);
+    }
+
+    @GetMapping(path = "/personnexml", produces = MediaType.APPLICATION_XML_VALUE)
+    ResponseEntity<PersonneBean> getPersonneXML() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(personneBean);
     }
 
 }
--- src/main/java/org/formation/projet3/personne/PersonneBean.java
+++ src/main/java/org/formation/projet3/personne/PersonneBean.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+
 @Component
 @Scope("singleton")
 //@Scope("prototype")
