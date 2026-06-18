6138ed16debe816abe15ad8ff3a6c9b990744b8d
Mohammed REZGUI <m.rezgui06@gmail.com>
2026-06-18 10:17:56 +0200

    feat: update home contoller

--- src/main/java/org/formation/projet3/controllers/HomeController.java
+++ src/main/java/org/formation/projet3/controllers/HomeController.java
@@ -1,6 +1,9 @@
 package org.formation.projet3.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +17,35 @@ public class HomeController {
     @GetMapping("/bonjour")
     public String bonjour() {
         return "Bonjour!!!";
+    }
+
+    @GetMapping("/bye")
+    public String bye() {
+        return "Goodbye!!!";
+    }
+
+    @GetMapping("/name")
+    public String name() {
+        return "Julien";
+    }
+
+    @GetMapping("/name2")
+    public ResponseEntity<String> name2() {
+        return ResponseEntity.ok("Julien");
+    }
+
+    @GetMapping("/created")
+    public ResponseEntity<String> created() {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body("Created");
+    }
+
+    @GetMapping("/notfound")
+    public ResponseEntity<String> notFound() {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("Ressource introuvable");
     }
 
 }
